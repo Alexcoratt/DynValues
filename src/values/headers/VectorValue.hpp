5c72c88e6723efc9f8988ed3cc3a7f619b0912d4
Alexander Smirnov <alexsm.w@yandex.ru>
2023-12-17 06:23:39 +0300

    Added new value type - vector

---
+++ src/values/headers/VectorValue.hpp
@@ -0,0 +1,41 @@
+#ifndef VECTOR_VALUE_HPP
+#define VECTOR_VALUE_HPP
+
+#include <vector>
+#include "IValue.hpp"
+
+class VectorValue : public IValue {
+private:
+	std::vector<IValue *> _values;
+
+public:
+	VectorValue(std::vector<IValue const *> const & values);
+	VectorValue(VectorValue const & other);
+	VectorValue & operator=(VectorValue const & other);
+	~VectorValue();
+
+	void swap(VectorValue & other);
+
+	VectorValue * getClone() const override;
+
+	bool operator<(IValue const &) const override;
+
+	operator std::string() const override;
+	operator double() const override;
+	operator int() const override;
+	operator unsigned long() const override;
+
+	IValue * operator[](std::size_t const & index);
+	IValue * at(std::size_t const & index);
+	IValue const * at(std::size_t const & index) const;
+	IValue * back();
+	IValue const * back() const;
+
+	void push_back(IValue const * value);
+	void pop_back();
+	std::size_t size() const;
+
+	bool isVector() const override { return true; }
+};
+
+#endif
---
+++ src/values/sources/VectorValue.cpp
@@ -0,0 +1,88 @@
+#include <algorithm>
+#include <sstream>
+
+#include "VectorValue.hpp"
+#include "UnableToTransformException.hpp"
+#include "IncompatibleValueTypesException.hpp"
+
+VectorValue::VectorValue(std::vector<IValue const *> const & values) : _values{values.size()} {
+	std::size_t len = _values.size();
+	for (std::size_t i = 0; i < len; ++i)
+		_values[i] = values.at(i)->getClone();
+}
+
+VectorValue::VectorValue(VectorValue const & other) : _values{other._values.size()} {
+	std::size_t len = _values.size();
+	for (std::size_t i = 0; i < len; ++i)
+		_values[i] = other._values.at(i)->getClone();
+}
+
+VectorValue & VectorValue::operator=(VectorValue const & other) {
+	if (this != &other) {
+		VectorValue tmp{other};
+		swap(tmp);
+	}
+	return *this;
+}
+
+VectorValue::~VectorValue() {
+	for (auto value : _values)
+		delete value;
+}
+
+void VectorValue::swap(VectorValue & other) {
+	std::swap(_values, other._values);
+}
+
+VectorValue * VectorValue::getClone() const {
+	std::size_t len = _values.size();
+	std::vector<IValue const *> res{len};
+	for (std::size_t i = 0; i < len; ++i)
+		res[i] = _values.at(i)->getClone();
+	return new VectorValue{res};
+}
+
+bool VectorValue::operator<(IValue const & other) const {
+	auto otherVector = dynamic_cast<VectorValue const *>(&other);
+	if (!otherVector)
+		throw IncompatibleValueTypesException("VectorValue", "not a VectorValue");
+	std::size_t len = _values.size();
+	std::size_t otherLen = otherVector->_values.size();
+	if (len != otherLen)
+		return len < otherLen;
+	for (std::size_t i = 0; i < len; ++i)
+		if (_values.at(i) != otherVector->at(i))
+			return _values.at(i) < otherVector->at(i);
+	return false;
+}
+
+VectorValue::operator std::string() const {
+	std::stringstream str;
+	str << '[';
+	std::size_t len = _values.size();
+	if (len != 0)
+		str << (std::string)*_values[0];
+	for (std::size_t i = 1; i < len; ++i)
+		str << ", " << (std::string)*_values[i];
+	str << ']';
+	return str.str();
+}
+
+VectorValue::operator double() const { throw UnableToTransformException("VectorValue", "DoubleValue"); }
+VectorValue::operator int() const { throw UnableToTransformException("VectorValue", "IntValue"); }
+VectorValue::operator unsigned long() const { throw UnableToTransformException("VectorValue", "UnsignedLongValue"); }
+
+IValue * VectorValue::operator[](std::size_t const & index) { return _values[index]; }
+IValue * VectorValue::at(std::size_t const & index) { return _values.at(index); }
+IValue const * VectorValue::at(std::size_t const & index) const { return _values.at(index); }
+IValue * VectorValue::back() { return _values.back(); }
+IValue const * VectorValue::back() const { return _values.back(); }
+
+void VectorValue::push_back(IValue const * value) { _values.push_back(value->getClone()); }
+void VectorValue::pop_back() {
+	IValue * value = _values.back();
+	_values.pop_back();
+	delete value;
+}
+
+std::size_t VectorValue::size() const { return _values.size(); }
